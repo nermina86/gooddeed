acef08214b8b062dd96022d282d8a2f37fffe480
Nermina <minamemisevic86@gmail.com>
2025-06-27 21:44:43 +0200

    added applewatch

---
+++ gooddeed/gooddeedswatch Watch App/gooddeedswatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  gooddeedswatchApp.swift
+//  gooddeedswatch Watch App
+//
+//  Created by Mina Memisevic on 27. 6. 2025..
+//
+
+import SwiftUI
+
+@main
+struct gooddeedswatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
